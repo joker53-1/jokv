eb464f6f073acf9f41987d6d9521c285ed72baa6
joker-53 <1205637346@qq.com>
2023-08-29 20:34:28 +0800

    p1 finished

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -11,26 +12,83 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := reader.GetCF(req.GetCf(), req.GetKey())
+	if err != nil {
+		return nil, err
+	}
+	if bytes == nil {
+		return &kvrpcpb.RawGetResponse{NotFound: true}, nil
+	}
+	return &kvrpcpb.RawGetResponse{Value: bytes}, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	put := storage.Put{
+		Key:   req.Key,
+		Value: req.Value,
+		Cf:    req.Cf,
+	}
+	modify := storage.Modify{
+		Data: put,
+	}
+	err := server.storage.Write(req.Context, []storage.Modify{modify})
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawPutResponse{}, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+	del := storage.Delete{
+		Key: req.Key,
+		Cf:  req.Cf,
+	}
+	modify := storage.Modify{
+		Data: del,
+	}
+	err := server.storage.Write(req.Context, []storage.Modify{modify})
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawDeleteResponse{}, nil
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return nil, err
+	}
+	iter := reader.IterCF(req.Cf)
+	count := uint32(0)
+	var kvs []*kvrpcpb.KvPair
+	iter.Seek(req.StartKey)
+	for i := iter.Item(); iter.Valid(); iter.Next() {
+		if count < req.Limit {
+			value, err := i.Value()
+			if err != nil {
+				return nil, err
+			}
+			kvs = append(kvs, &kvrpcpb.KvPair{
+				Error: nil,
+				Key:   i.Key(),
+				Value: value,
+			})
+		} else {
+			break
+		}
+	}
+	return &kvrpcpb.RawScanResponse{Kvs: kvs}, nil
 }
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,20 +1,60 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"os"
+	"path/filepath"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engine *engine_util.Engines
+	conf   *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dbPath := conf.DBPath
+	kvPath := filepath.Join(dbPath, "kv")
+	raftPath := filepath.Join(dbPath, "raft")
+
+	os.MkdirAll(kvPath, os.ModePerm)
+	os.MkdirAll(raftPath, os.ModePerm)
+
+	raftDB := engine_util.CreateDB(raftPath, true)
+	kvDB := engine_util.CreateDB(kvPath, false)
+	store := StandAloneStorage{
+		engine: engine_util.NewEngines(kvDB, raftDB, kvPath, raftPath),
+		conf:   conf,
+	}
+	return &store
+}
+
+type StandAloneReader struct {
+	txn *badger.Txn
+}
+
+func (s *StandAloneReader) GetCF(cf string, key []byte) ([]byte, error) {
+	value, err := engine_util.GetCFFromTxn(s.txn, cf, key)
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	}
+	return value, err
+}
+
+func (s *StandAloneReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, s.txn)
+}
+
+func (s *StandAloneReader) Close() {
+	s.txn.Discard()
+	return
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -29,10 +69,34 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	txn := s.engine.Kv.NewTransaction(false)
+	return &StandAloneReader{txn: txn}, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	for _, b := range batch {
+		switch b.Data.(type) {
+		case storage.Put:
+			put := b.Data.(storage.Put)
+			key := put.Key
+			value := put.Value
+			cf := put.Cf
+			err := engine_util.PutCF(s.engine.Kv, cf, key, value)
+			if err != nil {
+				return err
+			}
+			break
+		case storage.Delete:
+			del := b.Data.(storage.Delete)
+			key := del.Key
+			cf := del.Cf
+			err := engine_util.DeleteCF(s.engine.Kv, cf, key)
+			if err != nil {
+				return nil
+			}
+			break
+		}
+	}
 	return nil
 }
